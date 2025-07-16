d08cc850103f42ccad1df87c9b5ef8d1f45df227
guilhermeandrd <nickyandrade331@gmail.com>
2025-07-16 11:19:49 -0300

    limpeza da main

--- main.cpp
+++ main.cpp
@@ -6,30 +6,7 @@ using namespace std;
 int main() {
 
     
-    vector<int> restp;
-    
-    for(int i=0;i<7;i++){
-        restp.push_back(i);
-    }
-    
-    Heap<int> tchau(restp);
-    Heap <int> oi(restp);
-
-    cout << tchau.top() << endl;
-
-    while(!tchau.empty()){
-        cout << tchau.top() << " " ;
-        tchau.pop();
-    }
-
-    cout << endl;
-
-    oi.pop(2);
-
-    while(!oi.empty()){
-        cout << oi.top() << " " ;
-        oi.pop();
-    }
-
+    //vector<int> restp;
+    Heap<int> oi;
     return 0;
 }
